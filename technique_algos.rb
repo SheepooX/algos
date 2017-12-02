2a5f7f5fe16451c5e0dbfdb14cb34675fb83bd20
Tomáš Černý <tmscer@gmail.com>
2017-12-02 18:11:07 +0000

    Update technique_algos.rb

--- technique_algos.rb
+++ technique_algos.rb
@@ -5,7 +5,7 @@ class TechniqueAlgos
 		# Pointer from left right
 		i = 0
 		j = arr.length - 1
-		# Find the first pair two swap
+		# Find the first pair to swap
 		i += 1 until arr[i] % 2 == 1 or i >= arr.length - 1
 		j -= 1 until arr[j] % 2 == 0 or j < 1
 		# Until no pairs are found
